056724618f6aa5b9f1974cc30ecb2c5d2339a3a3
catalina <catalinaruedaf@hotmail.com>
2025-07-26 11:44:47 +1000

    Update Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,31 +18,31 @@ export default function Contact() {
         <FormsContact/>
        
         <section class="social section">
-    <div class="container">
-        <p>Follow us on social media for updates and news about our courses.</p>
-        <div class="social-icons">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Facebook">
-  <FaFacebook size={32} />
-</a>
-<a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Twitter">
-  <FaTwitter size={32} />
-</a>
-<a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Instagram">
-  <FaInstagram size={32} />
-</a>
-<a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Linkedin">
-  <FaLinkedin size={32} />
-</a>
-<a href="https://youtube.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Youtube">
-  <FaYoutube size={32} />
-</a>
-<a href="https://pinterest.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Pinterest">
-  <FaPinterest size={32} />
-</a>
-        </div>
-    </div>
-</section>
- </section>
+          <div class="container">
+            <p>Follow us on social media for updates and news about our courses.</p>
+              <div class="social-icons">
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Facebook">
+                  <FaFacebook size={32} />
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Twitter">
+                  <FaTwitter size={32} />
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Instagram">
+                  <FaInstagram size={32} />
+                </a>
+                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Linkedin">
+                      <FaLinkedin size={32} />
+                </a>
+                <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Youtube">
+                <FaYoutube size={32} />
+                </a>
+                <a href="https://pinterest.com" target="_blank" rel="noopener noreferrer" aria-label="Follow Us On Pinterest">
+                  <FaPinterest size={32} />
+                </a>
+              </div>
+            </div>
+          </section>
+        </section>
 
     );
 }
